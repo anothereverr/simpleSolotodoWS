91367e0cccd5d3f4c3248c5b0ffa9f39d318bbf2
disguiseddev <daniel.mendoza.caro@gmail.com>
2020-10-17 21:35:01 -0300

    feat: code rework and telegram message

--- main.py
+++ main.py
@@ -2,27 +2,51 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
+import requests
 
-DRIVER_PATH = "D:\\apps\\chromedriver\\chromedriver.exe"
-URL_SOLOTODO = "https://www.solotodo.cl/computer_cases?motherboard_formats=251395"
+def mainProgram():
 
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200") 
+    DRIVER_PATH = "D:\\apps\\chromedriver\\chromedriver.exe"
+    URL_SOLOTODO = "https://www.solotodo.cl/computer_cases?motherboard_formats=251395"
+    QUERY = "H200"
 
-driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1200") 
+
+    driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
 
-while True:
-    driver.get(URL_SOLOTODO)
-    content = driver.page_source
-    soup = BeautifulSoup(content, "html.parser")
+    while True:
+        driver.get(URL_SOLOTODO)
+        content = driver.page_source
+        soup = BeautifulSoup(content, "html.parser")
 
-    for element in soup.findAll('div',attrs={'class':'d-flex flex-column category-browse-result'}):
-        productName = element.findAll('a', href=True)[0].text
-        productPrice = element.findAll('div', attrs={'class': 'price flex-grow'})[0].text
-        print("Gabinete %s a %s  " % (productName, productPrice) )
+        # search for all elements and getting its name and price
+        for element in soup.findAll('div',attrs={'class':'d-flex flex-column category-browse-result'}):
+            productName = element.findAll('a', href=True)[0].text
+            productPrice = element.findAll('div', attrs={'class': 'price flex-grow'})[0].text
+            print("Producto %s a %s  " % (productName, productPrice) )
 
-    time.sleep(60)
+            if QUERY in productName:
+                sendTelegramAlert("Producto %s encontrado a un precio de %s " % (productName, productPrice) )
+                
 
-driver.quit()
+        time.sleep(60)
+    driver.quit()
+
+
+def sendTelegramAlert(message):
+
+    TOKEN = 'xxxx'
+    CHAT_ID = 'xxxx'
+   
+    send_text = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&parse_mode=Markdown&text=' + message
+
+    response = requests.get(send_text)
+    return response.json()
+
+
+mainProgram()
+
+
